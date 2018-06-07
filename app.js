5ffdc5e6496fe24c104b72500ae59add1b2144e6
Joseph Arias <arias.joseph.a@gmail.com>
2018-06-07 17:23:38 -0400

    Getting Input Data

--- app.js
+++ app.js
@@ -6,14 +6,36 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-
+  
+  const Domstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value'
+  };
+  
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(Domstrings.inputType).value,
+        description: document.querySelector(Domstrings.inputDescription).value,
+        value: document.querySelector(Domstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return Domstrings;
+    }
+  }
 })();
 
 // GlOBAL APP CONTROLLER
 var controller = (function(budgetCtrl,UICtrl) {
 
+  const DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function() {
     // Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
   }
 
   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
